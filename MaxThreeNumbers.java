f2c0915a6ef3d0557cc0935752da8409ce268a5b
GeekyPBhaumik <134827207+GeekyPBhaumik@users.noreply.github.com>
2024-01-06 13:14:44 +0530

    Update MaxThreeNumbers.java

--- MaxThreeNumbers.java
+++ MaxThreeNumbers.java
@@ -11,13 +11,23 @@ public class MaxThreeNumbers {
            return c;
        }
     }
+    static int smallestOfThree(int a,int b,int c){
+        if(a<b && a<c){
+            return a;
+        }else if(b<a && b<c){
+            return b;
+        }else{
+            return c;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        int max = maxOfThree(a,b,c);
-        System.out.println(max);
+        int smallest = smallestOfThree(a,b,c);
+        System.out.println("Largest is "+max);
+        System.out.println("Smallest is "+smallest);
     }
 }
